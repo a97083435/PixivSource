a3cb53168256298d7861f4f2fcf3d0b397ac1b0f
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-06-10 12:03:46 +0800

    Pixiv 搜索：备用书源不执行登录

--- pixiv/search.js
+++ pixiv/search.js
@@ -53,7 +53,11 @@ function isLogin() {
 function getUserNovels() {
     if (!isLogin()) {
         sleepToast("⚠️ 当前未登录账号\n\n请登录 Pixiv 账号")
-        source.login()
+        if (source.bookSourceName.includes("备用")) {
+            sleepToast('我的 - 书源管理 - 三点菜单 - 登录 - 登录账号')
+        } else {
+            source.login()
+        }
         return []
     }
 
