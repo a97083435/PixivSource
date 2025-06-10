8c360afa2ca374031c5bbbe900621734cbb070c5
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2025-06-10 09:22:00 +0800

    Pixiv 登录界面：重排登录界面

--- pixiv/base.loginUrl.js
+++ pixiv/base.loginUrl.js
@@ -19,7 +19,7 @@ function logout() {
     removeCookie()
     java.startBrowser("https://www.pixiv.net/logout.php", "退出账号")
     removeCookie()
-    sleepToast(`✅已退出当前账号\n\n退出后请点击右上角的 ✔️ 退出\n\n登录请点击【登录账号】进行登录`)
+    sleepToast(`✅ 已退出当前账号\n\n退出后请点击右上角的 ✔️ 退出\n\n登录请点击【登录账号】进行登录`)
 }
 
 function removeCookie() {
